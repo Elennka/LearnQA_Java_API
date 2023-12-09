dad69715fd1e3a319ea9cec2ca238a835da7eb63
Elennka <vean125@mail.ru>
2023-12-09 16:32:58 +0500

    Ex7: long redirect

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -7,16 +7,24 @@ public class HelloWorldTest {
     @Test
     public void testRestAssured(){
 
-        Response response = RestAssured
+        String locationHeader = "https://playground.learnqa.ru/api/long_redirect";
+        int statusCode;
+        int i = 1;
+
+        do {
+            Response response = RestAssured
                     .given()
                     .redirects()
                     .follow(false)
                     .when()
-                    .get("https://playground.learnqa.ru/api/long_redirect")
+                    .get(locationHeader)
                     .andReturn();
 
-            String locationHeader = response.getHeader("Location");
-            System.out.println("Редирект на: " + locationHeader);
-
+            locationHeader = response.getHeader("Location");
+            statusCode= response.getStatusCode();
+            if (statusCode != 200)
+                System.out.println("Редирект "+ i +": " + locationHeader);
+            i++;
+        } while ((statusCode!=200) && (locationHeader!=null));
     }
 }
