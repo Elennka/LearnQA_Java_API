04565e35a5e3660c5fe8277201e7bf781df4c585
Elennka <vean125@mail.ru>
2023-12-10 17:59:45 +0500

    Ex8: Tokens

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,30 +1,61 @@
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import io.restassured.path.json.JsonPath;
+
 import org.junit.jupiter.api.Test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 
 
 public class HelloWorldTest {
     @Test
     public void testRestAssured(){
 
-        String locationHeader = "https://playground.learnqa.ru/api/long_redirect";
-        int statusCode;
-        int i = 1;
+        String urlTask = "https://playground.learnqa.ru/ajax/api/longtime_job";
 
-        do {
-            Response response = RestAssured
-                    .given()
-                    .redirects()
-                    .follow(false)
-                    .when()
-                    .get(locationHeader)
-                    .andReturn();
+//создание задачи
+        JsonPath responseTask = RestAssured
+            .get(urlTask)
+            .jsonPath();
+        String token = responseTask.get("token");
+        int seconds = responseTask.get("seconds");
 
-            locationHeader = response.getHeader("Location");
-            statusCode= response.getStatusCode();
-            if (statusCode != 200)
-                System.out.println("Редирект "+ i +": " + locationHeader);
-            i++;
-        } while ((statusCode!=200) && (locationHeader!=null));
+//Запрос сразу после получения token
+
+        Map<String, String> params = new HashMap<>();
+        params.put("token",token);
+        JsonPath responseCheck = RestAssured
+               .given()
+               .queryParams(params)
+               .get(urlTask)
+               .jsonPath();
+
+        String status = responseCheck.getString("status");
+
+        if (status.equals("Job is NOT ready"))
+            System.out.println("Задача еще не готова, нужно подождать " + seconds + " сек");
+
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+// Запрос после истечени времени
+        responseCheck = RestAssured
+                .given()
+                .queryParams(params)
+                .get(urlTask)
+                .jsonPath();
+        String result = responseCheck.get("result");
+        status = responseCheck.get("status");
+        if (status.equals("Job is ready") && result!=null)
+            System.out.println("Задача готова, результат = " + responseCheck.get("result"));
+        else
+            System.out.println("По истечении " + seconds + " сек задача так и не готова");
     }
 }
